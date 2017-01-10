062596aef84197108f8b6b6ccb5d62c2a55994b4
Ethan Lin <ethan.lin@uwex.edu>
2017-01-10 15:37:24 -0600

    added the optional to turn Google Tracking on/off

--- README.md
+++ README.md
@@ -67,4 +67,4 @@ _see project list_
 
 ---
 ###License
-Storybook Plus (SB+) is licensed under [GNU v3](https://github.com/oel-mediateam/sbplus_v3/blob/master/LICENSE) license. Copyright (c) 2013-2016 Ethan S. Lin, [University of Wisconsin-Extension, Division of Continuing Education, Outreach & E-Learning](http://ce.uwex.edu/)
+Storybook Plus (SB+) is licensed under [GNU v3](https://github.com/oel-mediateam/sbplus_v3/blob/master/LICENSE) license. Copyright (c) 2013-2017 Ethan S. Lin, [University of Wisconsin-Extension, Division of Continuing Education, Outreach & E-Learning](http://ce.uwex.edu/)
--- sources/scripts/sbplus-dev.js
+++ sources/scripts/sbplus-dev.js
@@ -306,10 +306,23 @@ var SBPLUS = SBPLUS || {
                 sections: xSections
             };
             
+            // GET GOOGLE TRACKING
+            if ( this.xml.settings.analytics === 'on' ) {
+                
+                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+                
+                ga('create', self.manifest.sbplus_google_tracking_id, 'auto');
+                ga('send', 'pageview');
+                ga('send', 'event');
+            }
+            
+            // get centralized author name and profile
             var sanitizedAuthor = this.sanitize( xAuthor.attr( 'name' ).trim() );
             var profileUrl = this.manifest.sbplus_author_directory + sanitizedAuthor + '.json';
             
-            // get centralized author name and profile
             $.ajax( {
                     
                 crossDomain: true,
