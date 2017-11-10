6a2be4c58e9f10710c8b2e11d56188a46a70f9f2
Ethan Lin <ethan.lin@uwex.edu>
2017-11-10 10:34:26 -0600

    added hotjar tracking

--- sources/scripts/sbplus-dev.js
+++ sources/scripts/sbplus-dev.js
@@ -525,6 +525,19 @@ var SBPLUS = SBPLUS || {
             // get/set the presenation title
             self.uniqueTitle = self.sanitize( self.xml.setup.title );
             
+            // if HotJar site id is set in manifest, get and set HotJar tracking code
+            if (self.manifest.sbplus_hotjar_site_id != "") {
+                var id = Number(self.manifest.sbplus_hotjar_site_id);
+                (function(h,o,t,j,a,r){
+                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                    h._hjSettings={hjid:id,hjsv:6};
+                    a=o.getElementsByTagName('head')[0];
+                    r=o.createElement('script');r.async=1;
+                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                    a.appendChild(r);
+                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            }
+            
             // if analytics is on, get and set Google analtyics tracking
             if ( self.xml.settings.analytics === 'on' ) {
                 
